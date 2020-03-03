d4e3f73f79b3b15e9d548a5dae8725b50cee933c
WJLEE <wj.lee@barco.com>
2020-03-03 18:25:10 +0800

    fix docker $XDG_RUNTIME_DIR=/run/user/0

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,7 +4,7 @@ echo "PWD"=$PWD
 XDG_RUNTIME_DIR=/run/user/$(id -u)
 echo "XDG_RUNTIME_DIR"=${XDG_RUNTIME_DIR}
 
-sudo docker run -v $PWD:/root/.mycroft --device /dev/snd -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native -v ~/.config/pulse/cookie:/root/.config/pulse/cookie -p 8181:8181 --name mycroft mycroftai/docker-mycroft
+sudo docker run -v $PWD:/root/.mycroft --device /dev/snd -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:/run/user/0/pulse/native -v ~/.config/pulse/cookie:/root/.config/pulse/cookie -p 8181:8181 --name mycroft mycroftai/docker-mycroft
 sudo chown -R gitlab-runner:gitlab-runner $PWD/*
  
 exit 0
