04b435cc0c8ceeea65402f005e9c5899d4b58bbf
WJLEE <wj.lee@barco.com>
2020-03-03 16:58:34 +0800

    fix XDG_RUNTIME_DIR issue

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "PWD"=$PWD
+XDG_RUNTIME_DIR=/run/user/$(id -u)
 echo "XDG_RUNTIME_DIR"=${XDG_RUNTIME_DIR}
 
 sudo docker run -v $PWD:/root/.mycroft --device /dev/snd -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native -v ~/.config/pulse/cookie:/root/.config/pulse/cookie -p 8181:8181 --name mycroft mycroftai/docker-mycroft
