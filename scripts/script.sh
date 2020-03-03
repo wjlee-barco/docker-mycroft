e43166b7cbf800f333715a5a9fdb44a0cec411f1
WJLEE <wj.lee@barco.com>
2020-03-03 22:03:47 +0800

    1. add DBUS_SYSTEM_BUS_ADDRESS 2. add status checking for pulseaudio -vvvv

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,7 +4,7 @@ echo "PWD"=$PWD
 XDG_RUNTIME_DIR=/run/user/$(id -u)
 echo "XDG_RUNTIME_DIR"=${XDG_RUNTIME_DIR}
 
-sudo docker run -v $PWD:/root/.mycroft --device /dev/snd -e PULSE_SERVER=unix:/run/user/0/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:/run/user/0/pulse/native -v ~/.config/pulse/cookie:/root/.config/pulse/cookie -p 8181:8181 --name mycroft mycroftai/docker-mycroft
+sudo docker run -v $PWD:/root/.mycroft --device /dev/snd -e DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket -e PULSE_SERVER=unix:/run/user/0/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:/run/user/0/pulse/native -v ~/.config/pulse:/root/.config/pulse -p 8181:8181 --name mycroft mycroftai/docker-mycroft
 sudo chown -R gitlab-runner:gitlab-runner $PWD/*
  
 exit 0
--- scripts/script_status.sh
+++ scripts/script_status.sh
@@ -2,5 +2,6 @@
 
 sudo docker exec --tty mycroft env TERM=xterm env
 sudo docker exec --tty mycroft env TERM=xterm pactl info
+sudo docker exec --tty mycroft env TERM=xterm pulseaudio -vvvv
  
 exit 0
