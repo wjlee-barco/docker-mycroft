51a345a1b768df338a96cd4ad9090fa89fe3d5ef
WJLEE <wj.lee@barco.com>
2020-03-03 23:42:46 +0800

    make sure DBUS_SYSTEM_BUS_ADDRESS is exported

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,7 +4,9 @@ echo "PWD"=$PWD
 XDG_RUNTIME_DIR=/run/user/$(id -u)
 echo "XDG_RUNTIME_DIR"=${XDG_RUNTIME_DIR}
 
-sudo docker run -v $PWD:/root/.mycroft --device /dev/snd -e DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket -e PULSE_SERVER=unix:/run/user/0/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:/run/user/0/pulse/native -v ~/.config/pulse:/root/.config/pulse -p 8181:8181 --name mycroft mycroftai/docker-mycroft
+DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
+
+sudo docker run -v $PWD:/root/.mycroft --device /dev/snd -e DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/user/0/bus -e PULSE_SERVER=unix:/run/user/0/pulse/native -v ${XDG_RUNTIME_DIR}/pulse/native:/run/user/0/pulse/native -v ~/.config/pulse:/root/.config/pulse -p 8181:8181 --name mycroft mycroftai/docker-mycroft
 sudo chown -R gitlab-runner:gitlab-runner $PWD/*
  
 exit 0
